8e84f898ecce9b159df09cf941a8d405e4fbf715
Niklas Tinglöf <niklas.tinglof@devport.se>
2015-10-31 13:40:07 +0100

    - minors

--- src/light_node/light_node.ino
+++ src/light_node/light_node.ino
@@ -1,3 +1,7 @@
+/*
+  light_node.ino
+*/
+
 #include <SPI.h>
 //#include <mcp_can.h>
 //#include <mcp_can_dfs.h>
@@ -7,21 +11,20 @@
 #include <Adafruit_NeoPixel.h>
 #include <Adafruit_DotStar.h>
 
-
 /*
 
   todo: update light "areas" and behaviours...
   todo: update to handle scenarious and complex workings...
-  
+
   light node
   lighting behaviour needed/wanted:
-  - "static" color, used for dna strand, only subject to dimming and possibly "living" colors i.e. glimmering effect using random colors from palette 
+  - "static" color, used for dna strand, only subject to dimming and possibly "living" colors i.e. glimmering effect using random colors from palette
   - "static" color, used for strands, possibly more vivid than dna otherwise same or similar
   - "static" colors, used for mouths and eyes, probably only dimming with one static color per item
 
   communication could be by CAN as it can handle distance and is quite resistant to disturbances *AND* is bidirectional.
   but we will probably go for ttl usart as it is short haul between main controller and light controller
-  
+
   possibly separate applications distributed over several nodes for differens areas.
   in that case a lib should be developed and used.
   a big issue is the not very user-friendlyness of the arduino ide in that you cannot:
@@ -47,13 +50,13 @@ void setup()
   // - init datastructures...
 
   // todo: init comm stuff...
-  // - use usart 
+  // - use usart
 }
 
 void communication_handler(void);
 void command_handler(void);
 
-void loop() 
+void loop()
 {
   // put your main code here, to run repeatedly:
 
@@ -92,21 +95,21 @@ enum
 
 
 QueueList<uint8_t>        g_incoming_buffer;          // queue of bytes before assembly...
-QueueList<cmd_message_t>  g_incoming_command_queue; 
+QueueList<cmd_message_t>  g_incoming_command_queue;
 
 // todo: init...
 void communication_init(void)
 {
-//  Serial1.begin();  
+//  Serial1.begin();
 }
 
-// 
+//
 void communication_handler(void)
 {
   static uint8_t buffer_counter = 0;
   static uint8_t buffer[2*sizeof(cmd_message_t)];
   cmd_message_t* ptr = 0;
-  
+
   // copy to buffer...
   while ((buffer_counter < sizeof(cmd_message_t)) && Serial1.available())
   {
@@ -120,7 +123,7 @@ void communication_handler(void)
   }
 
   // todo: sanity check...
-  
+
   // todo: try to assemble packet..
   // todo: if successful, queue...
   ptr = (cmd_message_t*)buffer;
@@ -141,7 +144,4 @@ void command_handler(void)
 }
 
 
-// todo: 
-
-
-
+// todo:
