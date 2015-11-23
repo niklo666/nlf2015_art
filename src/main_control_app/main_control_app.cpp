216122fffd5e757a9c978dceb22f862da8c3948f
Niklas Tinglöf <niklas.tinglof@devport.se>
2015-11-23 12:44:34 +0100

    - started implementing socket works for main control app...

--- src/main_control_app/main_control_app.cpp
+++ src/main_control_app/main_control_app.cpp
@@ -2,6 +2,7 @@
   main_control_app.cpp
 
   command line application to control the main node as well as connected nodes...
+  add a choice to enter interactive mode to run multiple consecutive commands
 
   example:
   <app> light n r g b - set color/intensity (r,g,b) of light source n
@@ -15,6 +16,9 @@
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
 #include <syslog.h>
 #include <pthread.h>
 
@@ -23,31 +27,110 @@ using namespace std;
 void help(void)
 {
   cout << "help:\r\n";
-  cout << "<app name> light [id] [r],[g],[b] - set light intensity/color (r,g,b) of light source id\r\n";
-  cout << "<app name> light [id] 0 - short for light source off\r\n";
-  cout << "<app name> light [id] 255 - short for light source full on\r\n";
-  cout << "<app name> camera [id] image - acquire, download image and view from camera\r\n";
-  cout << "<app name> start   - \r\n";
-  cout << "<app name> stop    - \r\n";
-  cout << "<app name> status  - \r\n";
+  cout << "<app name> <host> <port> light [id] [r],[g],[b] - set light intensity/color (r,g,b) of light source id\r\n";
+  cout << "<app name> <host> <port> light [id] 0 - short for light source off\r\n";
+  cout << "<app name> <host> <port> light [id] 255 - short for light source full on\r\n";
+  cout << "<app name> <host> <port> camera [id] image - acquire, download image and view from camera\r\n";
+  cout << "<app name> <host> <port> start   - \r\n";
+  cout << "<app name> <host> <port> stop    - \r\n";
+  cout << "<app name> <host> <port> status  - \r\n";
 }
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
+
+int socket_init(char* hostname, int portno);
 
 int main(int argc, char* argv[])
 {
   // todo: parse command line...
-  if (argc == 1)
+  if (argc == 1 || (strcmp(argv[1], "-h") == 0) || (strcmp(argv[1], "help") == 0))
   {
+    help();
+    return 0;
+  }
+  else if (argc < 4)
+  {
+    cout << "error: too few arguments...\r\n";
     help();
     return -1;
   }
 
-  if (strcmp(argv[1], "light") == 0)
+  if (strcmp(argv[3], "light") == 0)
   {
   }
-  else if (strcmp(argv[1], "camera") == 0)
+  else if (strcmp(argv[3], "camera") == 0)
   {
   }
 
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+  char buffer[256];
+
+  // todo: sanity check...
+  portno = atoi(argv[2]);
+
+  sockfd = socket_init(argv[1], portno);
+  if (sockfd < 0)
+  {
+    return -1;
+  }
+
+  bzero(buffer,256);
+
+  // todo: implement the actual filling of message contents...
+
+  n = write(sockfd, buffer, strlen(buffer));
+  if (n < 0)
+  {
+    error("ERROR writing to socket");
+  }
+
+  bzero(buffer,256);
+  n = read(sockfd,buffer,255);
+  if (n < 0)
+  {
+    error("ERROR reading from socket");
+  }
+
+  printf("%s\n",buffer);
+  close(sockfd);
+
+  return 0;
+}
+
+int socket_init(char* hostname, int portno)
+{
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
+  //char buffer[256];
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0)
+  {
+    error("ERROR opening socket");
+  }
+
+  server = gethostbyname(hostname);
+  if (server == NULL)
+  {
+      fprintf(stderr,"ERROR, no such host\n");
+      exit(0);
+  }
+
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+  serv_addr.sin_port = htons(portno);
+  if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+  {
+    error("ERROR connecting");
+  }
 
   return 0;
 }
