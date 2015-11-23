08d785629d1f9feb0bd074d23794a17fd9cf9852
Niklas Tinglöf <niklas.tinglof@devport.se>
2015-11-23 10:10:00 +0100

    - clean up

--- src/main_node/comm.cpp
+++ src/main_node/comm.cpp
@@ -10,7 +10,8 @@
   - status inquiries...
 */
 
-#include "../../shared/main_protocol.h"
+//#include "../../shared/camera_protocol.h"
+#include "../../shared/camera_protocol.h"
 #include "comm.h"
 
 void* comm_thread_function(void* ptr)
