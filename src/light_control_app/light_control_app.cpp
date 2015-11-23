77730b8d9f7e0136298c6e34ab5c8bbc9e54aea6
Niklas Tinglöf <niklas.tinglof@devport.se>
2015-11-23 10:21:48 +0100

    - started on main control app...

---
+++ src/light_control_app/light_control_app.cpp
@@ -0,0 +1,6 @@
+/*
+  light_control_app.cpp
+
+  application to control the light node thru the main node or not!!!s
+
+*/
---
+++ src/main_control_app/main_control_app.cpp
@@ -0,0 +1,16 @@
+/*
+  main_control_app.cpp
+
+  command line application to control the main node as well as connected nodes...
+
+  example:
+  <app> light n r g b - set color/intensity (r,g,b) of light source n
+  <app> camera n image  - get image from camera node n
+*/
+
+
+int main(int argc, char* argv[])
+{
+
+  return 0;
+}
